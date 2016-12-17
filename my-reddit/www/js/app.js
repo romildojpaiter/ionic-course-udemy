37345bf982fd0f62a6cb220927a39cb24e0e9ab4
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-12-17 11:16:33 -0300

    Adicionando Infinite Schroll ao projeto

--- my-reddit/www/js/app.js
+++ my-reddit/www/js/app.js
@@ -5,13 +5,35 @@
 
     $scope.stories = [];
 
-    $http.get("https://www.reddit.com/r/Android/new/.json")
-      .success(function(response){
-        angular.forEach(response.data.children, function(child) {
-          $scope.stories.push(child.data);
+    function loadStories(params, callback) {
+        $http.get("https://www.reddit.com/r/Android/new/.json", {params: params})
+          .success(function(response){
+            var stories = [];
+            angular.forEach(response.data.children, function(child) {
+              $scope.stories.push(child.data);
+            });
+            callback(stories);
         });
-      });
+    }
 
+    $scope.loadOlderStories = function() {
+        var params = {};
+        if ($scope.stories.length > 0) {
+            params['after'] = $scope.stories[$scope.stories.length - 1].name;
+        }
+        loadStories(params, function(olderStories) {
+            $scope.stories = $scope.stories.concat(olderStories);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
+
+    $scope.loadNewerStories = function() {
+        var params = {'before': $scope.stories[0].name};
+        loadStories(params, function(newStories) {
+            $scope.stories = newStories.concat($scope.stories);
+            $scope.$broadcast('scroll.refreshComplete');
+        })
+    };
 
   });
 
