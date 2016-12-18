4bc0f2ccaebdf08c8513e030170cdd71ce737785
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-12-18 09:36:18 -0300

    Adicionado rotas, multiplas telas e passagem de parametros

--- my-notes/www/js/app.js
+++ my-notes/www/js/app.js
@@ -1,43 +1,74 @@
-var app = angular.module('starter', ['ionic'])
+(function(){
 
-app.controller('ListCtrl', function($scope) {
+    var app = angular.module('starter', ['ionic']);
 
-    $scope.notes = [
+    var notes = [
         {
+            id: '1',
             title: 'First Note',
             description: 'This is first note'
         },
         {
+            id: '2',
             title: 'Second Note',
             description: 'This is second note'
         }
     ];
-});
 
-app.config(function($stateProvider, $urlRouterProvider) {
+    function getNote(noteId) {
+        for (var i = 0; i < notes.length; i++) {
+            if (notes[i].id === noteId) {
+                return notes[i];
+            }
+        }
+        return undefined;
+    }
 
-    $stateProvider.state('list', {
-        url: '/list',
-        templateUrl: 'templates/list.html'
+    function updateNote(note) {
+        for (var i = 0; i < notes.length; i++) {
+            if (notes[i].id === note.id) {
+                notes[i] = note;
+                return;
+            }
+        }
+    }
+
+    app.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('list', {
+            url: '/list',
+            templateUrl: 'templates/list.html'
+        });
+        $stateProvider.state('edit', {
+            url: '/edit/:noteId',
+            templateUrl: 'templates/edit.html'
+        });
+        $urlRouterProvider.otherwise('/list');
     });
 
-    $stateProvider.state('edit', {
-        url: '/edit',
-        templateUrl: 'templates/edit.html'
+    app.controller('EditCtrl', function($scope, $state) {
+        $scope.note = angular.copy(getNote($state.params.noteId));
+
+        $scope.save = function() {
+            updateNote($scope.note);
+            $state.go('list');
+        };
+
     });
 
-    $urlRouterProvider.otherwise('/list');
+    app.controller('ListCtrl', function($scope) {
+        $scope.notes = notes;
+    });
 
-});
+    app.run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+          StatusBar.styleDefault();
+        }
+      });
+    });
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
+}());
