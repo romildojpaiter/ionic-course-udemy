0a674230fa46549379bfaad49ce98f50f0137daf
Romildo Jozue Paiter <romildo.paiter@sigma.com.br>
2016-12-26 12:20:10 -0300

    Entendendo as promesses

---
+++ my-promisses/www/js/app.js
@@ -0,0 +1,38 @@
+(function() {
+
+  var app = angular.module('app', ['ionic']);
+
+  app.controller('AppCtrl', function($scope, $timeout) {
+
+    function add(x, y) {
+      return $timeout(function() {
+        return x + y;
+      }, 100);
+    }
+
+    var startTime = Date.now();
+    add(5, 2)
+      .then(function(result) {
+        return add(result, 3);
+      })
+      .then(function(result) {
+        $scope.result = result;
+        $scope.elapsedTime = Date.now() - startTime;
+      });
+
+  });
+
+
+  // app.run(function($ionicPlatform) {
+  //   $ionicPlatform.ready(function() {
+  //     if(window.cordova && window.cordova.plugins.Keyboard) {
+  //       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  //       cordova.plugins.Keyboard.disableScroll(true);
+  //     }
+  //     if(window.StatusBar) {
+  //       StatusBar.styleDefault();
+  //     }
+  //   });
+  // });
+
+}());
